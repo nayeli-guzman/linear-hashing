b9be134c5126534393df416ae254260978be0e3f
nayeli <155994804+nayeli-guzman@users.noreply.github.com>
2024-07-09 20:39:01 -0500

    Update screen.h

--- screen.h
+++ screen.h
@@ -14,11 +14,8 @@ using namespace sf;
 class Screen {
 
     RenderWindow window;
-    Texture *image;
-    Sprite *square;
     Menu<> *menu;
     Linear_Hashing<>* table;
-
 
 public:
     Screen() {
@@ -39,35 +36,65 @@ public:
             while (window.pollEvent(event)) {
                 if (event.type == Event::Closed)
                     window.close();
+
                 if (event.type == Event::TextEntered) {
                     if (event.text.unicode>=48 && event.text.unicode <= 57 || event.text.unicode==8) {
                         if (event.text.unicode == 8 && menu->string_dynamic.getSize()>0)
                             menu->string_dynamic.erase(menu->string_dynamic.getSize()-1, 1);
-                        else
+                        else if (event.text.unicode != 8)
                             menu->string_dynamic += static_cast<char>(event.text.unicode);
 
                         menu->text_dynamic.setString(menu->string_dynamic);
                     }
 
                     if (event.text.unicode == 13 && menu->string_dynamic.getSize()>0) {
-                        string key_str = menu->text_dynamic.getString();
+                        string key_str = menu->text_dynamic.getString(); // captar el str
                         int key = stoi(key_str);
-                        table->insert(key);
+                        table->insert(key); //
+
                         menu->text_dynamic.setString("");
                         menu->string_dynamic = "";
                     }
+                    // falta borrar elementos
+                    // falta buscar
                 }
+
+                if (event.type == Event::MouseButtonPressed && event.mouseButton.button== Mouse::Left) {
+                    if (menu->b_insert.getGlobalBounds().contains(menu->mousePosition) && menu->string_dynamic.getSize()>0) {
+                        string key_str = menu->text_dynamic.getString();
+                        try { // catch backspace error
+                            int key = stoi(key_str);
+                            table->insert(key);
+                            menu->text_dynamic.setString("");
+                            menu->string_dynamic = "";
+                        }
+                        catch (exception &e) {
+                        }
+                    }
+                    else if (menu->b_delete.getGlobalBounds().contains(menu->mousePosition) && menu->string_dynamic.getSize()>0) {
+                        string key_str = menu->text_dynamic.getString();
+                        try { // catch backspace error
+                            int key = stoi(key_str);
+                            table->borrar(key);
+                            menu->text_dynamic.setString("");
+                            menu->string_dynamic = "";
+                        }
+                        catch (exception &e) {
+                        }
+                    }
+                }
+
             }
 
 
 
 /*
 
-*/
+*/          menu->update_mouse_position();
             menu->draw();
 
  //           window.draw(*square);
-            window.display();
+            //window.display();
 
         }
 
